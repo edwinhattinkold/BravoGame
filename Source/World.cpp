f35bec317b8800aae759b4f4e086b6de7315df3b
Ivan Horn <ivanhorn1990@gmail.com>
2015-12-22 13:04:02 +0100

    memory leak in world reset.

--- Source/World.cpp
+++ Source/World.cpp
@@ -181,6 +181,7 @@ void World::destroyPlayableContent()
 	delete updateContainer;							updateContainer = nullptr;
 	delete contactHandler;							contactHandler = nullptr;
 	delete center;									center = nullptr;
+	delete hud;										hud = nullptr;
 }
 
 void World::reset()
