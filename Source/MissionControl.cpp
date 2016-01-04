a43c654e145c1d496724ff3dfa68a6836048cfea
Ivan Horn <ivanhorn1990@gmail.com>
2016-01-04 14:26:33 +0100

    super mooi sterretje daarzo

--- Source/MissionControl.cpp
+++ Source/MissionControl.cpp
@@ -5,7 +5,7 @@ MissionControl::MissionControl()
 	Mission *mission = new Mission( "Go be useful" );
 	mission->addObjective( "Collect gasoline tanks ", Asset_Gasoline, 2 );
 	mission->addObjective( "Kill a tree ", Asset_Tree, 1 );
-	mission->addObjective( "Fuck that turret up ", Asset_Turret_Calm, 1 );
+	mission->addObjective( "F*ck that turret up ", Asset_Turret_Calm, 1 );
 	currentMission = mission;
 }
 
