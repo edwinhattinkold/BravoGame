37386e3774d3e0ba5ef3852c350736b710db3736
Erik Brandsma <erik.brandsma@outlook.com>
2016-01-06 19:53:29 +0100

    Final versie voor de presentatie

--- Source/MachineGun.cpp
+++ Source/MachineGun.cpp
@@ -1,7 +1,7 @@
 #include "MachineGun.h"
 
 MachineGun::MachineGun( World* newWorld, B2Content* host, b2World* physics_world, SDL_Renderer * renderTarget, float fireRate, float spread)
-	: Weapon( newWorld, host, physics_world, renderTarget, 15.0f, spread )
+	: Weapon( newWorld, host, physics_world, renderTarget, 19.0f, spread )
 {
 	Projectile* projectile = new Projectile( newWorld, physics_world, renderTarget, Asset_MachineGun_Bullet, 50, 100 );
 	setAmmo( projectile );
